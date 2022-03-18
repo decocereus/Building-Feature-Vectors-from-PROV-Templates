6de23c0c392dbde57d7ac0ff72c1b89dbf49e772
decocereus <amartyasinghkings07@gmail.com>
2022-03-18 14:59:09 +0000

    built the structure of the project

---
+++ index.py
@@ -0,0 +1,6 @@
+# run - here i will call everything and make the program run
+# typeExtractor - this will get the types from the template, i can read the template and get the types
+# getTypeFrequency - this will create a hashtable where type is key and its frequency is value
+# createFeatureVector - Build feature vector
+# buildSVM - build svm from feature vector
+# machineLearningAlgo - call this after the svm has been created
