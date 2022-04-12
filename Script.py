702914ef56a467afa1a31fdc1730a7b7aa9ad787
decocereus <amartyasinghkings07@gmail.com>
2022-04-12 18:07:40 +0100

    restructured the project

--- Script.py
+++ Script.py
@@ -1,3 +1,34 @@
 # import propertyOrder and other functions
 # run those functions here
 # can include the execution time of the whole process
+
+import json
+
+
+def getPropertyOrder():
+    f = open('files/propertyOrder.json')
+    data = json.load(f)
+    propertyOrder = []
+    for i in data['var']:
+        propertyOrder.append(i)
+    f.close()
+    return propertyOrder
+
+
+def getBindings(depth):
+    pass
+
+
+def getRelations():
+    with open('files/template.provn') as f:
+        for line in f:
+            if line.startswith('wasDerivedFrom'):
+                rels = line.split('(')
+                for relation in rels:
+                    if relation.startswith('var'):
+                        print(relation.split(':'))
+
+
+if __name__ == "__main__":
+    order = getPropertyOrder()
+    print(order)
--- index.py
+++ index.py
@@ -7,29 +7,43 @@
 # for each line of csv file and then count the number of occurence of each dictonary
 # the function which does this calculation is called the feature vector ?
 
-# def featureVector(depth):
-# allDicts = []
-# for i in range(depth):
-# currentDict = getSuccessorNodesDict(line)
-# allDicts.append(currentDict)
-# the calculate how many dictonaries are same
 
-
-# ask about the knowledge of the reader
+# ask about the knowledge of the reader- define everything in detail
 # how to best define provenance types
-# for feature vector do i have to count the frequency of each of hashmap built by the csv files
-# ask about the design of the project
+# for feature vector do i have to count the frequency of each of hashmap built by the csv files - no
+# ask about the design of the project - update the design and email him
 
 # Another approach for counting the frequncey can be
 # I can put the properties which are related to one another and put them in a hashmap
 # and everytime I encounter another occurence of that relation then I can just
 # incerease the freq in the hashmap
 
-# lets say we have property 4 relating to prop 9
-# then in the hashmap we have
-# dict = {'4-9': 1, '7-9': 5}
+# to get the types of all the nodes we start at level 0, for level 0 the template gives us the type for everything.
+# I have to get these types for everything like activity, entity, operation etc.
+# then I have to look at the realtions so lets say that
+# e2 wasDerivedFrom e1 then type1 of e2 = type0(e1) which is entity in this case
+
+# In order to get the types for each and every node i have iterated over all of them and then create a set for each node.
+# the set contains the types associated with each node.
+
+# then we iterate over all the sets and calculate the frequency of each set. This frequency is our feature vector.
+
+# then we are done
 
 
-# for design
+# for report
+# get chapter 2-4 by 12th April
 
-# getPropertyOrder
+# goal for tomorrow
+# write down the pseudo code and complete the first section of background
+
+# write down the implementation pseudo code
+
+# build template extractor
+# templateDict = {entity: [player_before, player_after, pokestop_before, pokestop_after],
+#                 activity: [operation],
+#                  wasDerivedFrom: }
+
+# def getEntities(template):
+# def getActivities(template):
+# def getRelations(template):
